56e1ce0afdc89008967ec0c5f1840812546d206c
Mathieu Desrochers <mathieu@desrochers>
2012-06-27 11:52:30 -0400

    Issue 10001 - Layout the DataAccessComponents
    Why bother ?

---
+++ DataAccessComponents.Benchmark/Program.cs
@@ -0,0 +1,132 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleHttpApplication.DataAccessComponents.Benchmark
+{
+    public class Program
+    {
+        /// <summary>
+        /// Performs a sequence of blocking database operations.
+        /// </summary>
+        private static void BlockingDatabaseOperations()
+        {
+            // Build the database connection.
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=localhost;Initial Catalog=SampleHttpApplication;Integrated Security=True"))
+            {
+                // Open the database connection.
+                sqlConnection.Open();
+
+                // Insert the database row.
+                SqlCommand insertSqlCommand = new SqlCommand("INSERT INTO [Session] VALUES('" + Guid.NewGuid() + "', 'Session Alpha', '2012-06-27 10:05:45'); SELECT CAST(SCOPE_IDENTITY() AS INT);", sqlConnection);
+                int sessionID = (int)insertSqlCommand.ExecuteScalar();
+
+                // Select the database row.
+                SqlCommand selectSqlCommand = new SqlCommand("SELECT * FROM [Session] WHERE [SessionID] = " + sessionID, sqlConnection);
+                SqlDataReader sqlDataReader = selectSqlCommand.ExecuteReader();
+                sqlDataReader.Read();
+                sqlDataReader.Close();
+
+                // Update the database row.
+                SqlCommand updateSqlCommand = new SqlCommand("UPDATE [Session] SET [SessionCode] = '" + Guid.NewGuid() + "', [Name] = 'Session Beta', [StartDate] = '2013-07-28 11:06:46' WHERE [SessionID] = " + sessionID, sqlConnection);
+                updateSqlCommand.ExecuteNonQuery();
+
+                // Delete the database row.
+                SqlCommand deleteSqlCommand = new SqlCommand("DELETE FROM [Session] WHERE [SessionID] = " + sessionID, sqlConnection);
+                updateSqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Performs a sequence of non blocking database operations.
+        /// </summary>
+        private async static Task NonBlockingDatabaseOperations()
+        {
+            // Build the database connection.
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=localhost;Initial Catalog=SampleHttpApplication;Integrated Security=True"))
+            {
+                // Open the database connection.
+                await sqlConnection.OpenAsync();
+
+                // Insert the database row.
+                SqlCommand insertSqlCommand = new SqlCommand("INSERT INTO [Session] VALUES('" + Guid.NewGuid() + "', 'Session Alpha', '2012-06-27 10:05:45'); SELECT CAST(SCOPE_IDENTITY() AS INT);", sqlConnection);
+                int sessionID = (int)await insertSqlCommand.ExecuteScalarAsync();
+
+                // Select the database row.
+                SqlCommand selectSqlCommand = new SqlCommand("SELECT * FROM [Session] WHERE [SessionID] = " + sessionID, sqlConnection);
+                SqlDataReader sqlDataReader = await selectSqlCommand.ExecuteReaderAsync();
+                await sqlDataReader.ReadAsync();
+                sqlDataReader.Close();
+
+                // Update the database row.
+                SqlCommand updateSqlCommand = new SqlCommand("UPDATE [Session] SET [SessionCode] = '" + Guid.NewGuid() + "', [Name] = 'Session Beta', [StartDate] = '2013-07-28 11:06:46' WHERE [SessionID] = " + sessionID, sqlConnection);
+                await updateSqlCommand.ExecuteNonQueryAsync();
+
+                // Delete the database row.
+                SqlCommand deleteSqlCommand = new SqlCommand("DELETE FROM [Session] WHERE [SessionID] = " + sessionID, sqlConnection);
+                await updateSqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        /// <summary>
+        /// Performs the benchmark on the specified database operations.
+        /// </summary>
+        private static void BenchmarkDatabaseOperations(string strategyName, Task[] databaseOperations)
+        {
+            // Start timing the database operations.
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            TimeSpan initialTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime;
+
+            // Wait for the database operations to complete.
+            Task.WaitAll(databaseOperations);
+
+            // Stop timing the database operations.
+            TimeSpan finalTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime;
+            stopwatch.Stop();
+
+            // Calculate the CPU utilization.
+            double duration = stopwatch.Elapsed.TotalMilliseconds;
+            double totalProcessorTime = finalTotalProcessorTime.TotalMilliseconds - initialTotalProcessorTime.TotalMilliseconds;
+            double cpuUtilization = ((totalProcessorTime / duration) * 100) / Environment.ProcessorCount;
+
+            // Display the benchmark results.
+            Console.WriteLine(strategyName);
+            Console.WriteLine("Duration: " + (int)duration + " milliseconds");
+            Console.WriteLine("CPU utilization: " + (int)cpuUtilization + "%");
+        }
+
+        /// <summary>
+        /// Runs when the program is started.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            // Prepare the blocking database operations.
+            Task[] blockingDatabaseOperations = new Task[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                blockingDatabaseOperations[i] = Task.Run(() => { BlockingDatabaseOperations(); });
+            }
+
+            // Benchmark the blocking database operations.
+            BenchmarkDatabaseOperations("Hang on to the current thread", blockingDatabaseOperations);
+            Console.WriteLine();
+
+            // Prepare the non blocking database operations.
+            Task[] nonBlockingDatabaseOperations = new Task[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                nonBlockingDatabaseOperations[i] = NonBlockingDatabaseOperations();
+            }
+
+            // Benchmark the non blocking database operations.
+            BenchmarkDatabaseOperations("Resume on another thread", nonBlockingDatabaseOperations);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 ## Design goals
 
+This sample application aims at wasting no CPU cycles on waiting
+for IO operations to complete.
+
+Whenever a piece of code encounters a blocking operation, it will
+relinquish the thread it is currently executing on. Then, once the
+blocking operation has completed, the code will resume its
+execution on another thread.
+
 ## Why bother?
+
+Let's conduct a simple benchmark on 1,000 sets of database operations
+scheduled for parallel execution. These operations will be the
+insertion, selection, update and deletion of a database row.
+
+### Strategy A - Hang on to the current thread
+
+Number of operations: 1,000  
+Total duration: 4917 milliseconds  
+CPU utilization: 8 %  
+
+### Strategy B - Resume on another thread
+
+Number of operations: 1,000  
+Total duration: 2778 milliseconds  
+CPU utilization: 19 %
+
+We can see that the benefits of not making the CPUs wait are twofold.
+
+First, they can start more concurrent IO requests, which reduces
+the time required to perform the complete set of database operations.
+
+Second, they are able to execute other lines of code, which augments
+the CPU utilization and makes better use of the available hardware.
