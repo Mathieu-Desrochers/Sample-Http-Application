dcecabf6de30023c693f4b37b80ef0406d33d34e
Mathieu Desrochers <mathieu@desrochers>
2012-07-03 12:20:55 -0400

    Issue 10004 - Layout the ServiceComponents
    Added the database connection provider.

---
+++ DataAccessComponents.Code/DatabaseConnectionProvider.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SampleHttpApplication.DataAccessComponents.Interface;
+
+namespace SampleHttpApplication.DataAccessComponents.Code
+{
+    /// <summary>
+    /// Represents the database connection provider.
+    /// </summary>
+    public class DatabaseConnectionProvider : IDatabaseConnectionProvider
+    {
+        /// <summary>
+        /// Opens a database connection.
+        /// </summary>
+        public IDatabaseConnection OpenDatabaseConnection()
+        {
+            // Get the configured connection string.
+            ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings["SampleHttpApplication"];
+            string connectionString = connectionStringSettings.ConnectionString;
+
+            // Build the database connection.
+            IDatabaseConnection databaseConnection = new DatabaseConnection(connectionString);
+            databaseConnection.Open();
+
+            // Return the database connection.
+            return databaseConnection;
+        }
+    }
+}
---
+++ DataAccessComponents.Interface/IDatabaseConnectionProvider.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleHttpApplication.DataAccessComponents.Interface
+{
+    /// <summary>
+    /// Represents the database connection provider.
+    /// </summary>
+    public interface IDatabaseConnectionProvider
+    {
+        /// <summary>
+        /// Opens a database connection.
+        /// </summary>
+        IDatabaseConnection OpenDatabaseConnection();
+    }
+}
