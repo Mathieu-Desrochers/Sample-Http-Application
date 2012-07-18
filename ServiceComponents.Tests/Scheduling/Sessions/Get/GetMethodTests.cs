06ae59174c861bd8f8008c62bc7d8bbf3d83c702
Mathieu Desrochers <mathieu@desrochers>
2012-07-18 16:04:37 -0400

    Issue 10004 - Layout the ServiceComponents
    Better expected JSON.

--- ServiceComponents.Tests/Scheduling/Sessions/Get/GetMethodTests.cs
+++ ServiceComponents.Tests/Scheduling/Sessions/Get/GetMethodTests.cs
@@ -47,9 +47,14 @@ namespace SampleHttpApplication.ServiceComponents.Tests.Scheduling.Sessions.Get
             // Verify the mocked components.
             testHarness.VerifyMockedComponents();
 
+            // Build the expected JSON content.
+            StringBuilder expectedJsonContent = new StringBuilder();
+            expectedJsonContent.Append("[");
+            expectedJsonContent.Append("]");
+
             // Validate the HTTP response message content.
             string httpResponseMessageContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
-            Assert.AreEqual("[]", httpResponseMessageContent);
+            Assert.AreEqual(expectedJsonContent.ToString(), httpResponseMessageContent);
         }
 
         /// <summary>
@@ -88,9 +93,102 @@ namespace SampleHttpApplication.ServiceComponents.Tests.Scheduling.Sessions.Get
             // Verify the mocked components.
             testHarness.VerifyMockedComponents();
 
+            // Build the expected JSON content.
+            StringBuilder expectedJsonContent = new StringBuilder();
+            expectedJsonContent.Append("[");
+            expectedJsonContent.Append("{");
+            expectedJsonContent.Append("\"SessionID\":10001,");
+            expectedJsonContent.Append("\"SessionCode\":\"Session-A\",");
+            expectedJsonContent.Append("\"Name\":\"Session Alpha\",");
+            expectedJsonContent.Append("\"StartDate\":\"2001-01-01T00:00:00\"");
+            expectedJsonContent.Append("}");
+            expectedJsonContent.Append("]");
+
             // Validate the HTTP response message content.
             string httpResponseMessageContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
-            Assert.AreEqual("[{\"SessionID\":10001,\"SessionCode\":\"Session-A\",\"Name\":\"Session Alpha\",\"StartDate\":\"2001-01-01T00:00:00\"}]", httpResponseMessageContent);
+            Assert.AreEqual(expectedJsonContent.ToString(), httpResponseMessageContent);
+        }
+
+        /// <summary>
+        /// Should return multiple Session resources.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnMultipleSessionResources()
+        {
+            // Build the test harness.
+            SessionsControllerTestHarness testHarness = new SessionsControllerTestHarness();
+
+            // Mock the invocation of the GetSessions business operation.
+            testHarness.MockedSchedulingBusinessLogicComponent
+                .Setup(mock => mock.GetSessions(It.IsAny<IDatabaseConnection>(), It.IsAny<GetSessionsBusinessRequest>()))
+                .Returns(Task.FromResult(new GetSessionsBusinessResponse()
+                {
+                    // Mock the Session business response elements.
+                    Sessions = new GetSessionsBusinessResponse.SessionBusinessResponseElement[]
+                    {
+                        // Mock the Session business response element.
+                        new GetSessionsBusinessResponse.SessionBusinessResponseElement()
+                        {
+                            SessionID = 10001,
+                            SessionCode = "Session-A",
+                            Name = "Session Alpha",
+                            StartDate = new DateTime(2001, 1, 1)
+                        },
+
+                        // Mock the Session business response element.
+                        new GetSessionsBusinessResponse.SessionBusinessResponseElement()
+                        {
+                            SessionID = 10002,
+                            SessionCode = "Session-B",
+                            Name = "Session Bravo",
+                            StartDate = new DateTime(2002, 2, 2)
+                        },
+
+                        // Mock the Session business response element.
+                        new GetSessionsBusinessResponse.SessionBusinessResponseElement()
+                        {
+                            SessionID = 10003,
+                            SessionCode = "Session-C",
+                            Name = "Session Charlie",
+                            StartDate = new DateTime(2003, 3, 3)
+                        }
+                    }
+                }))
+                .Verifiable();
+
+            // Invoke the HTTP GET method.
+            HttpRequestMessage httpRequestMessage = testHarness.BuildHttpRequest(HttpMethod.Get, "api/scheduling/sessions", "application/json");
+            HttpResponseMessage httpResponseMessage = testHarness.HttpClient.SendAsync(httpRequestMessage).Result;
+
+            // Verify the mocked components.
+            testHarness.VerifyMockedComponents();
+
+            // Build the expected JSON content.
+            StringBuilder expectedJsonContent = new StringBuilder();
+            expectedJsonContent.Append("[");
+            expectedJsonContent.Append("{");
+            expectedJsonContent.Append("\"SessionID\":10001,");
+            expectedJsonContent.Append("\"SessionCode\":\"Session-A\",");
+            expectedJsonContent.Append("\"Name\":\"Session Alpha\",");
+            expectedJsonContent.Append("\"StartDate\":\"2001-01-01T00:00:00\"");
+            expectedJsonContent.Append("},");
+            expectedJsonContent.Append("{");
+            expectedJsonContent.Append("\"SessionID\":10002,");
+            expectedJsonContent.Append("\"SessionCode\":\"Session-B\",");
+            expectedJsonContent.Append("\"Name\":\"Session Bravo\",");
+            expectedJsonContent.Append("\"StartDate\":\"2002-02-02T00:00:00\"");
+            expectedJsonContent.Append("},");
+            expectedJsonContent.Append("{");
+            expectedJsonContent.Append("\"SessionID\":10003,");
+            expectedJsonContent.Append("\"SessionCode\":\"Session-C\",");
+            expectedJsonContent.Append("\"Name\":\"Session Charlie\",");
+            expectedJsonContent.Append("\"StartDate\":\"2003-03-03T00:00:00\"");
+            expectedJsonContent.Append("}");
+            expectedJsonContent.Append("]");
+
+            // Validate the HTTP response message content.
+            string httpResponseMessageContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            Assert.AreEqual(expectedJsonContent.ToString(), httpResponseMessageContent);
         }
     }
 }
